7a1a060355c9e48e34459cad04db1b820f0df1bf
Mohit Arora <marora@buffalo.edu>
2014-02-09 15:31:32 -0500

    Fixed Issues Changed Splash screen to use Asynctask and fetch feed data on first page Passing data through Intents

--- src/deal/with/it/Welcome.java
+++ src/deal/with/it/Welcome.java
@@ -1,10 +1,25 @@
 package deal.with.it;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.view.Window;
+import android.util.Log;
 
 public class Welcome extends Activity{
 	/*
@@ -12,13 +27,18 @@ public class Welcome extends Activity{
 	private Button prevButton;
 	private ImageView imgView;
 	private Intent intent;
-	
+	*/
+	  String readTwitterFeed, postdate[]=new String[2],excerpts[]=new String[4],dates[]=new String[4];
 	@Override
 	public void onCreate(Bundle bundle){
 		super.onCreate(bundle);
-		initializeLayout();
-	}
-
+		setContentView(R.layout.home);
+		new PrefetchData().execute();
+		}
+	
+//		initializeLayout();
+//	}
+/*
 	private void initializeLayout() {
 		setContentView(R.layout.home);
 //		okButton=(Button)findViewById(R.id.ok);
@@ -39,7 +59,7 @@ public class Welcome extends Activity{
 			break;
 		}
 	}*/
-	
+	/*
 	 private CountDownTimer lTimer;
 
 	    public void onCreate(Bundle savedInstanceState) {
@@ -62,7 +82,83 @@ public class Welcome extends Activity{
 	        Intent lIntent = new Intent();
 	        lIntent.setClass(this, today.class);
 	        startActivity(lIntent);
+	        Log.i("deal.with.it","Done");
 	        finish();
 	    }
+*/
+	private class PrefetchData extends AsyncTask<Void, Void, Void> {
+		 
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // before making http calls         
+ 
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+        	readTwitterFeed = readTwitterFeed();
+        	try{
+        		JSONObject jsonObject = new JSONObject(readTwitterFeed);
+        		JSONArray posts =(JSONArray)jsonObject.getJSONArray("posts");
+        		for(int j=0;j<4;j++){
+        			JSONObject x=posts.getJSONObject(j);
+        			excerpts[j]=x.getString("excerpt").split("<div class=\"sharedaddy")[0].split("</div><br/><p>")[1];
+        			postdate=x.getString("date").split(" ");
+//        			System.out.println(postdate[0]);
+        			dates[j]=postdate[0];
+        			System.out.println(dates[j]);
+        		}
+        	}
+        	catch(Exception e){
+        		readTwitterFeed=null;
+        		postdate[0]=null;
+        		e.printStackTrace();
+        	}
+        	Log.i("deal.with.it",readTwitterFeed);
+        	return null;    
 
+        }
+        private String readTwitterFeed() {
+    		StringBuilder builder = new StringBuilder();
+    	    HttpClient client = new DefaultHttpClient();
+    	    Log.i("deal.with.it","Read twitter feed");
+    	    HttpGet httpGet = new HttpGet("http://lotusmeditationgroup.com/?cat=4&json=1");
+    	    try {
+    	      HttpResponse response = client.execute(httpGet);
+    	      StatusLine statusLine = response.getStatusLine();
+    	      int statusCode = statusLine.getStatusCode();
+    	      if (statusCode == 200) {
+    	        HttpEntity entity = response.getEntity();
+    	        InputStream content = entity.getContent();
+    	        BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+    	        String line;
+    	        Log.i(today.class.toString(),"Here inside the status code");
+    	        while ((line = reader.readLine()) != null) {
+    	          builder.append(line);
+    	        }
+    	      } else {
+    	        Log.e(today.class.toString(), "Failed to download file");
+    	      }
+    	    } catch (ClientProtocolException e) {
+    	      e.printStackTrace();
+    	    } catch (IOException e) {
+    	      e.printStackTrace();
+    	    }
+    	    Log.i(today.class.toString(),"Returning string");
+    	    return builder.toString();
+    	}
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            Intent lIntent = new Intent();
+	        lIntent.setClass(Welcome.this, today.class);
+	        lIntent.putExtra("feed",excerpts);
+	        lIntent.putExtra("date", dates);
+	        startActivity(lIntent);
+	        Log.i("deal.with.it","Done");
+	        finish();
+            
+        }
+	}
+	
 }
--- src/deal/with/it/today.java
+++ src/deal/with/it/today.java
@@ -30,47 +30,72 @@ import android.widget.TextView;
 
 public class today extends Activity implements OnClickListener{
 	private TextView tv1;
-	private Button bt1;
+	private Button bt1,bt2,bt3;
 	static String[] jsoncat={"status","count","count_total","pages","posts"};
-	
+	private String feed[]=new String[3],date[]=new String[3],excerpt;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    Bundle extras = getIntent().getExtras();
+    feed= extras.getStringArray("feed");
+    date= extras.getStringArray("date");
+    Log.i("deal.with.it","----");
+//    Log.i("deal.with.it",feed);
+//    Log.i("deal.with.it",date);
+    Log.i("deal.with.it","----");
     initLayout();
 	}
 	private void initLayout(){
 		setContentView(R.layout.today);
-		bt1 =(Button)findViewById(R.id.bt1);
-		tv1=(TextView)findViewById(R.id.tv1);
-	    bt1.setOnClickListener((OnClickListener) this);	
+		Log.i("deal.with.it","Done again");
+		bt1 =(Button)findViewById(R.id.Button01);
+		tv1=(TextView)findViewById(R.id.TextView02);
+		bt1.setOnClickListener((OnClickListener) this);	
+		bt2=(Button)findViewById(R.id.Button02);
+		bt3=(Button)findViewById(R.id.bt1);
+		Calendar c = Calendar.getInstance();
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		String formattedDate = df.format(c.getTime());
+		for(int i=0;i<4;i++){
+
+			if(date[i].equals(formattedDate))
+			{
+				Log.i("deal.with.it","date was equal");
+				excerpt=feed[i];
+				bt1.setVisibility(0);
+				Log.i("deal.with.it",excerpt);
+				break;
+			}
+			else
+			{
+				Log.i("deal.with.it","date was not equal");
+//				bt1.setVisibility(4);
+				tv1.setVisibility(0);
+			}
+		}
 	}
 	@Override
 	public void onClick(View v){  
     Log.i("webtry","layout now trying");
-    tv1.setVisibility(0);
-//    tv1.append("Sometext");
+//    tv1.setVisibility(0);
     String readTwitterFeed = readTwitterFeed();
-    //Log.i("deal.with.it",readTwitterFeed);
     try {
       JSONObject jsonObject = new JSONObject(readTwitterFeed);
       JSONArray posts =(JSONArray)jsonObject.getJSONArray("posts");
-//      for (int j = 0; j < posts.length(); j++) {
       	Log.i("deal.with.it","Starting posts to string");
       	Calendar c = Calendar.getInstance();
       	System.out.println("Current time => " + c.getTime());
 
       	SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
       	String formattedDate = df.format(c.getTime());
-   	       //Log.i("deal.with.it",posts.toString(2));
       	JSONObject x=posts.getJSONObject(0);
-      	System.out.println(x);
+      	System.out.println(x.getString("excerpt").split("<div class=\"sharedaddy")[0].split("</div><br/><p>")[1]);
       	String[] postdate=x.getString("date").split(" ");
         Log.i("deal.with.it",formattedDate+" "+postdate[0]);
       	Log.i("deal.with.it",x.getString("id"));
    	    if(postdate[0].equals(formattedDate))
    	    {
       	Log.i("deal.with.it","Todays post");
-      	//tv1.append(x.getString("url"));
       	String url=x.getString("url");
       	Intent i = new Intent(Intent.ACTION_VIEW);
       	i.setData(Uri.parse(url));
@@ -79,18 +104,16 @@ public class today extends Activity implements OnClickListener{
    	    else
    	    {
    	    	Log.i("deal.with.it","Should I show some old posts");
-//   	    	tv1.append(x.getString("content"));
    	    	String url=x.getString("url");
    	      	Intent i = new Intent(Intent.ACTION_VIEW);
    	      	i.setData(Uri.parse(url));
    	      	startActivity(i);
    	    }
-//      }
     } catch (Exception e) {
       e.printStackTrace();
     }
   }
-
+	
 	private String readTwitterFeed() {
 		StringBuilder builder = new StringBuilder();
 	    HttpClient client = new DefaultHttpClient();
